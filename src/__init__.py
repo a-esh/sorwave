3d99a3a13a459d658c5e36256a093b4cb4091d44
Abraham Escalona <abrahamescalona@live.com>
2024-05-31 12:37:03 +0200

    Delete src directory

--- src/__init__.py
+++
@@ -1,5 +0,0 @@
-from metadata_manager import get_metadata
-from music_logger import gen_log
-from music_logger import new_log
-from music_logger import filter_artist
-from song_sorter import sort_songs
