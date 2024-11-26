1ada28e18008635e231158c56babba5e8b23ada7
A-esh <abrahamescalona@live.com>
2024-11-26 09:16:14 +0100

    Refactor logger functions to improve artist filtering and logging structure

--- sorwave/logger.py
+++ sorwave/logger.py
@@ -6,27 +6,13 @@ from .musicbrainzngs_API import set_useragent
 import musicbrainzngs
 from progress.bar import Bar
 
-def filter_artist(artist, title = None, use_api= False):
-    if use_api:
-        set_useragent()
-        try:
-            if title:
-                result = musicbrainzngs.search_recordings(artist=artist, recording=title, limit=1)
-                if result['recording-list']:
-                    artist = result['recording-list'][0]['artist-credit'][0]['artist']['name']
-            else:
-                result = musicbrainzngs.search_artists(artist=artist, limit=1)
-                if result['artist-list']:
-                    artist = result['artist-list'][0]['name']
-        except musicbrainzngs.WebServiceError as e:
-            print(f"MusicBrainz API error: {e}")
-    else:
-        dividers = [",", "/", "Ft.", "feat.", "Feat", "&"]
-        for fix in dividers:
-            artist = artist.split(fix)[0].strip()
+def get_main_artist(artist):
+    dividers = [",", "/", "Ft.", "feat.", "Feat", "&"]
+    for fix in dividers:
+        artist = artist.split(fix)[0].strip()
     return artist
 
-def new_log_file(folder_path, log_dict, log_type, sorter_log):
+def new_log_file(folder_path, log_dict, log_type, sorter_log=None):
     date = datetime.datetime.now().strftime("%Y-%m-%d %H-%M-%S")
     log_path = os.path.join(folder_path, f"{log_type} {date.replace(':', '-')}.json")
     log_info = {
@@ -47,55 +33,57 @@ def new_log_file(folder_path, log_dict, log_type, sorter_log):
     if os.name == 'nt':
         os.system(f'attrib +h "{log_path}"')
 
-def gen_log(folder_path, use_api=False, gen_log=True):
+def gen_log(path, library_path=False , gen_log=True):
+    def process_file(file_path, song_log, bugs_log):
+        song_metadata = get_metadata(file_path)
+        if not song_metadata:
+            bugs_log["Metadata error"] = file_path
+        else:
+            artist = song_metadata.get("albumartist", song_metadata.get("artist"))
+            artist = get_main_artist(artist)
+            album = song_metadata.get("album", song_metadata.get("title"))
+            album = album.replace("/", "-")
+            song_metadata["tracknumber"] = song_metadata.get("tracknumber", 1)
+
+            if artist not in song_log:
+                song_log[artist] = {}
+            if album not in song_log[artist]:
+                song_log[artist][album] = []
+            song_log[artist][album].append({
+                "artist": song_metadata.get("artist"),
+                "albumartist": song_metadata.get("albumartist"),
+                "title": song_metadata["title"],
+                "tracknumber": str(song_metadata["tracknumber"]).split("/")[0],
+                "genre": song_metadata.get("genre"),
+                "path": file_path,
+                "extension": os.path.splitext(file_path)[1],
+            })
+
     song_log = {}
     bugs_log = {}
-    
-    total_files = sum([len(files) for r, d, files in os.walk(folder_path) if any(f.endswith(('.flac', '.mp3')) for f in files)])
-    bar = Bar('Processing', max=total_files)
 
-    # Iterate through files and update the progress bar
-    for root, dirs, files in os.walk(folder_path):
-        for file in files:
-            if file.endswith('.flac') or file.endswith('.mp3'):
-                bar.next()
-                file_path = os.path.join(root, file)
-                song_metadata = get_metadata(file_path)
-                if not song_metadata:
-                    bugs_log["Metadata error"] = file_path
-                else:
-                    artist = song_metadata.get("albumartist", song_metadata.get("artist"))
-                    artist = filter_artist(artist, song_metadata["title"], use_api)
-                    album = song_metadata.get("album", song_metadata.get("title"))
-                    album = album.replace("/", "-")
-                    song_metadata["tracknumber"] = song_metadata.get("tracknumber", 1)
+    if not library_path:
+        library_path = path
 
-                    # Add "Modified by sorwave" tag to metadata
-                    if use_api:
-                        song_metadata["modified_by"] = "sorwave"
 
-                    if artist not in song_log:
-                        song_log[artist] = {}
-                    if album not in song_log[artist]:
-                        song_log[artist][album] = []
-                    song_log[artist][album].append({
-                        "artist": song_metadata.get("artist"),
-                        "albumartist": song_metadata.get("albumartist"),
-                        "title": song_metadata["title"],
-                        "tracknumber": str(song_metadata["tracknumber"]).split("/")[0],
-                        "genre": song_metadata.get("genre"),
-                        "path": file_path,
-                        "extension": os.path.splitext(file_path)[1],
-                        "modified_by": "None"
-                    })
-                    if use_api:
-                        song_log[artist][album].append({
-                            "modified_by": "sorwave"
-                    })
+    if os.path.isdir(path):
+        total_files = sum([len(files) for r, d, files in os.walk(path) if any(f.endswith(('.flac', '.mp3')) for f in files)])
+        bar = Bar('Processing', max=total_files)
+
+        for root, dirs, files in os.walk(path):
+            for file in files:
+                if file.endswith('.flac') or file.endswith('.mp3'):
+                    bar.next()
+                    file_path = os.path.join(root, file)
+                    process_file(file_path, song_log, bugs_log)
+
+        bar.finish()
+    elif os.path.isfile(path) and path.endswith(('.flac', '.mp3')):
+        process_file(path, song_log, bugs_log)
 
     if gen_log:
-        new_log_file(folder_path, song_log, "songs_log")
+        new_log_file(library_path, song_log, "songs_log")
     if bugs_log:
-        new_log_file(folder_path, bugs_log, "metadataBugs_log")
+        new_log_file(library_path, bugs_log, "metadataBugs_log")
 
     return song_log
