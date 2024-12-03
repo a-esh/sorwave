6b54267fdc2a4caee5101d07b47f38cc10b78438
A-esh <abrahamescalona@live.com>
2024-12-03 13:25:49 +0100

    Add musicbrainzngs API functions and set user agent for improved metadata queries

--- sorwave/__init__.py
+++ sorwave/__init__.py
@@ -2,3 +2,4 @@ from sorwave.sorter import sort_song, sort_library, add_playlist, new_playlist
 from sorwave.logger import get_main_artist, new_log_file, generate_log
 from sorwave.metadata_extractor import get_metadata
 from sorwave.metadata_fixer import fix_metadata, previw_fix_metadata, recover_metadata
+from sorwave.musicbrainzngs_API import set_useragent, musicbrainzngs_query, musicbrainzngs_json_to_dict, musicbrainzngs_json_to_metadata
--- sorwave/musicbrainzngs_API.py
+++ sorwave/musicbrainzngs_API.py
@@ -1,3 +1,13 @@
 import musicbrainzngs
+import json
+
 def set_useragent():
     musicbrainzngs.set_useragent("Sorwave", "1.0", "abrahamescalona@live.com")
+
+def musicbrainzngs_query(file_name):
+    query = {'recording': file_name, 'limit': 1, 'fmt': 'json'}
+    return query
+
+def musicbrainzngs_json_to_metadata(data):
+    metadata = data['recording-list'][0]
+    return metadata
